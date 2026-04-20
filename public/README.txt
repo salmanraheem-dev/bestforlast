78f102c45f132bfef799016fabb2e35a7059b202
salmanraheem-dev <salmanraheem.dev@gmail.com>
2026-04-20 15:03:28 +0530

    fix: Buffer polyfill for WebKit, add public/ logo support

---
+++ public/README.txt
@@ -0,0 +1,2 @@
+Place your logo.png file in this folder.
+It will be served at: https://bestforlast.vercel.app/logo.png
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,17 @@
 import { defineConfig } from "vite";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 export default defineConfig({
+  plugins: [
+    // Polyfills Buffer, process, global etc. for TronWeb in browser (WebKit/Safari)
+    nodePolyfills({
+      globals: {
+        Buffer:  true,
+        global:  true,
+        process: true,
+      },
+    }),
+  ],
   server: {
     port: 3000,
     host: true,
